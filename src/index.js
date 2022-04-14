9617563aa6f5e4fde705792f0577485401b51523
Zhoucai <nizhoucai@gmail.com>
2022-04-13 22:02:17 -0400

    ready for deploy

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('starting up!');
+// change require to es6 import style
+import $ from 'jquery';
+import './style.scss';
+
+// program to display a text using setInterval method
+let num = 0;
+const track = () => {
+  num += 1;
+  $('#main').html(`You've been on this page for ${num} seconds.`);
+};
+
+setInterval(track, 1000);
---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const env = process.env.NODE_ENV || 'development';
+// set to 'production' or 'development' in your env
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const finalCSSLoader = (env === 'production') ? MiniCssExtractPlugin.loader : { loader: 'style-loader' };
+const autoprefixer = require('autoprefixer');
+const postcssPresets = require('postcss-preset-env');
+
+module.exports = {
+  mode: env,
+  devServer: {
+    hot: true,
+  },
+  entry: ['./src'], // this is where our app lives
+  devtool: 'source-map', // this enables debugging with source in chrome devtools
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'babel-loader' },
+        ],
+      },
+      {
+        test: /\.s?css/,
+        use: [
+          finalCSSLoader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            ident: 'postcss',
+            options: {
+              sourceMap: true,
+              postcssOptions: {
+                plugins: [
+                  autoprefixer(),
+                  postcssPresets({ browsers: 'last 2 versions' }),
+                ],
+              },
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      }, {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/[name].[contenthash][ext][query]',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    new ESLintPlugin({}),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+      favicon: './src/favicon.png',
+    }),
+  ],
+};
